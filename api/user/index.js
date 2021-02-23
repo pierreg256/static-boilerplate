a7f2c3a630e0ef846edcda8fc4a46d2bc599ac75
pierreg256 <pierre.gilot@me.com>
2021-02-23 11:14:07 +0100

    adding graphql

--- api/user/index.js
+++ api/user/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 const header = req.headers["x-ms-client-principal"];
@@ -11,23 +10,3 @@ const header = req.headers["x-ms-client-principal"];
     }
   };
 }
-=======
-module.exports = async function (context, req) {
-  context.log("JavaScript HTTP trigger function processed a request.");
-
-  const header = req.headers["x-ms-client-principal"] || "";
-  const encoded = Buffer.from(header, "base64");
-  let decoded = {};
-  try {
-    decoded = JSON.parse(encoded.toString("ascii"));
-  } catch (error) {
-    decoded = {};
-  }
-
-  context.res = {
-    body: {
-      clientPrincipal: decoded,
-    },
-  };
-};
->>>>>>> 9e9de4d9679b6b60b62da569cd0cb302e66b6022
